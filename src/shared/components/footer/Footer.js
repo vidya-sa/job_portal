a62ea994027a8354028968e89b18b2b4c5a88d43
vidya-sa <75720079+vidya-sa@users.noreply.github.com>
2021-05-17 17:48:21 +0530

    Update Footer.js
    command on links

--- src/shared/components/footer/Footer.js
+++ src/shared/components/footer/Footer.js
@@ -8,13 +8,13 @@ function Footer() {
         <div className="footer">
             <div className="links">
             <h4>Social Media Links</h4>
-            <a href="#" className="fa fa-facebook"></a>
-            <a href="#" className="fa fa-twitter"></a>
-            <a href="#" className="fa fa-google"></a>
-            <a href="#" className="fa fa-linkedin"></a>
-            <a href="#" className="fa fa-youtube"></a>
-            <a href="#" className="fa fa-instagram"></a>
-            <a href="#" clasName="fa fa-pinterest"></a>
+//             <a href="#" className="fa fa-facebook"></a>
+//             <a href="#" className="fa fa-twitter"></a>
+//             <a href="#" className="fa fa-google"></a>
+//             <a href="#" className="fa fa-linkedin"></a>
+//             <a href="#" className="fa fa-youtube"></a>
+//             <a href="#" className="fa fa-instagram"></a>
+//             <a href="#" clasName="fa fa-pinterest"></a>
 </div>
         </div>
     )
